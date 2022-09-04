cd677cc838d89184189480b06f207e1c7cd0259b
Ryan Thomas <ryanthomasj15@gmail.com>
2022-09-04 15:33:47 +0100

    Add css and display messages.

--- game.js
+++ game.js
@@ -11,17 +11,17 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-  console.log('Player chose ' + playerChoice);
-  console.log('Computer chose ' + computerChoice);
+  showInfo('You chose ' + playerChoice);
+  showInfo('Computer chose ' + computerChoice);
 
   if (playerChoice === victorMap.get(computerChoice)) {
-    console.log('You win this round.');
+    showInfo('You win this round.');
     return 1;
   } else if (playerChoice === computerChoice) {
-    console.log('This round is a tie');
+    showInfo('This round is a tie');
     return 0;
   } else {
-    console.log('You lost this round.');
+    showInfo('You lost this round.');
     return -1;
   }
 }
@@ -41,34 +41,45 @@ function selectScissors() {
   playRound('scissors', computerChoice);
 }
 
-function game() {
-  //initialise score
-  let score = 0;
+function showInfo(info) {
+  const content = document.createElement('div');
+  content.classList.add('content');
+  content.textContent = info;
 
-  //play 5 rounds, getting a new player choice each time
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
-    let computerChoice = getComputerChoice();
-
-    let roundResult = playRound(playerChoice, computerChoice)
-    score += roundResult;
-  }
-
-  //check score and declare winner
-  if (score > 0) {
-    console.log("You are victorious!");
-  } else if (score === 0) {
-    console.log("It's a tie.");
-  } else {
-    console.log("You have been defeated.");
-  }
+  gameInfo.appendChild(content);
 }
+
+// function game() {
+//   //initialise score
+//   let score = 0;
+
+//   //play 5 rounds, getting a new player choice each time
+//   for (let i = 0; i < 5; i++) {
+//     let playerChoice = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+//     let computerChoice = getComputerChoice();
+
+//     let roundResult = playRound(playerChoice, computerChoice)
+//     score += roundResult;
+//   }
+
+//   //check score and declare winner
+//   if (score > 0) {
+//     console.log("You are victorious!");
+//   } else if (score === 0) {
+//     console.log("It's a tie.");
+//   } else {
+//     console.log("You have been defeated.");
+//   }
+// }
 
 // game();
 
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
+
+const gameInfo = document.querySelector('.gameinfo');
+const scoreBoard = document.querySelector('.scoreboard');
 
 rockButton.addEventListener('click', selectRock);
 paperButton.addEventListener('click', selectPaper);
